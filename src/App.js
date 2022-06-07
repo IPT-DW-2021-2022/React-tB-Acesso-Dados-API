05f53f306c5edabda46645d17aa8314b2afb9091
jcnpereira <casimiro@ipt.pt>
2022-06-07 17:29:17 +0100

    Adição do componente <Tabela />

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@
  */
 
 import React from "react";
+import Tabela from "./Tabela";
 
-class App extends React.Component{
+class App extends React.Component {
 
-  render(){
+  state = {
+    // esta lista de animais há-de receber dados da API
+    animais: []
+  }
+
+/**
+ * Tarefas
+ *   1. ler dados da API
+ *   2. atribuir esses dados à variável state 'animais'
+ */
+
+
+  render() {
+    // ler os dados do STATE
+    const { animais } = this.state;
+
     return (
-      <div className="container"></div>
+      <div className="container">
+        <h1>Animais</h1>
+        {/* 
+        <h4>Novo animal:</h4>
+        <Formulario /> */}
+
+        <br />
+        <h4>Lista de animais</h4>
+        <Tabela dadosAnimaisIN={animais} />
+
+        <br /><br />
+      </div>
     )
   }
 }
